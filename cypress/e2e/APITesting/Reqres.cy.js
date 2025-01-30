277e267200a7ff0839120bbd809d18e9922b0f38
hozdemir33 <hozdemir8233@gmail.com>
2025-01-29 21:22:52 -0800

    first commit

---
+++ cypress/e2e/APITesting/Reqres.cy.js
@@ -0,0 +1,50 @@
+describe("Get Request", ()=>{
+
+ it("Testing Get Request from Reqres", ()=>{
+
+       cy.request("GET","https://reqres.in/api/users/2")
+         .its('status')
+         .should("equal", 200);
+
+     })
+
+     it("PUT CALL", ()=>{
+
+     cy.request( {
+
+             method:"PUT" ,
+             url :"https://reqres.in/api/users/2" ,
+             body: 
+             {
+                    "name": "hasan" ,
+                    "id"  :  2
+             }
+
+     } )
+     .its('status')
+     .should("equal", 200);
+
+})
+
+it("POST REQUEST", ()=>{
+
+    cy.request( {
+
+            method:"POST" ,
+            url :"https://reqres.in/api/users" ,
+            body: 
+            {
+            
+                    name: "hUSEYIN",
+                    id: "333"
+                
+            }
+
+    } )
+    .its('status')
+    .should("equal", 201);
+
+
+})
+
+})
