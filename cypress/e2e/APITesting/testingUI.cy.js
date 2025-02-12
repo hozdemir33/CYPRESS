66d5afa64eda18eb5bfd38b2cf65fe01f7f5fa0f
hozdemir33 <hozdemir8233@gmail.com>
2025-02-11 21:47:30 -0800

    second commit

--- cypress/e2e/APITesting/testingUI.cy.js
+++ cypress/e2e/APITesting/testingUI.cy.js
@@ -1,23 +1,26 @@
 describe("Testing UI", ()=>{
 
-    it("Testing", ()=>{
+    it("Testing_1", ()=>{
 
         cy.visit("https://example.cypress.io/")
           .contains("Commands")
          
-    
-
         })
-
-        it("Testing_1", ()=>{
+           it("Testing_2", ()=>{
 
             cy.visit("https://example.cypress.io/commands/querying")
             cy.contains("Button")
-           
-             
 
-    
             })
+
+            it("Testing_3", ()=>{
+                
+                cy.visit("https://example.cypress.io/commands/querying")
+                cy.get("[class='query-list']")
+                .contains('apples')
+    
+                })
           
+
         
         })
