aea29c36bf4519f8a91d8931e378cf2f4fd2c0b4
hozdemir33 <hozdemir8233@gmail.com>
2025-02-12 14:04:10 -0800

    first commit

--- cypress/e2e/APITesting/testingUI.cy.js
+++ cypress/e2e/APITesting/testingUI.cy.js
@@ -20,7 +20,50 @@ describe("Testing UI", ()=>{
                 .contains('apples')
     
                 })
-          
 
+                it("Testing_4", ()=>{
+                
+                    cy.visit("https://example.cypress.io/commands/querying")
+                    cy.get("[placeholder='Email']").type('fakeemails@email.com')
+                    cy.get("[id='inputPassword']").type("1234566609820930iuiuhiuhiuhiuhiuh29u@")
+                
+            
         
-        })
+                    })
+                   
+                    it ("HTTP GET REQUEST TESTING", ()=>{
+
+                       cy.request({
+                        url : "https://jsonplaceholder.typicode.com/posts"
+                       }).then((response)   => {
+                        expect(response.status).to.eq(200)
+
+                       })
+
+                    it("http get a single users", ()=>{
+                        cy.request({
+                            url :"https://jsonplaceholder.typicode.com/posts/1"
+                        }).then((response, ()=>{
+                          expect(response.status).to.eq(200)
+
+                        }
+                        
+                        ))
+
+
+                    })
+
+
+
+                           
+             })
+
+            
+            })
+
+ 
+
+            
+
+
+
