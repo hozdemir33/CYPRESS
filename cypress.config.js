b2e5c371a1c32b15a250fcbb07f0a8ac17c985df
hozdemir33 <hozdemir8233@gmail.com>
2025-02-25 21:47:04 -0800

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports =defineConfig ({
   
   e2e: {
     
+    failOnStatusCode: true,   //Adding this comment here to make sure CI/CD ALSO FAILS 
+
+
+
     setupNodeEvents(on, config) {
        
       on("before:browser:launch", ()=>{
